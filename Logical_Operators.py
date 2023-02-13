b303ecece39f83503795b0363e9323518c21c59a
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-13 09:45:59 +0530

    Add files via upload

---
+++ Logical_Operators.py
@@ -0,0 +1,8 @@
+# Logical Operators
+
+bool1 = True
+bool2 = False
+
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is", (bool1 or bool2))
+print("The value of not bool2 is", (not bool2))
