7bd8220290204bd6fa2c44f1a0d809ffa1262d83
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-13 09:44:17 +0530

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,9 @@
+a = 8
+b = 2
+
+# Arithmetic Operators
+
+print("The value of 3+4 is ", a+b)
+print("The value of 3-4 is ", a-b)
+print("The value of 3*4 is ", a*b)
+print("The value of 3/4 is ", a/b)
