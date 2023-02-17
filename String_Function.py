79fbbaf19dc8254a4e32d296e11322b479e2aca3
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-17 09:46:36 +0530

    Add files via upload

---
+++ String_Function.py
@@ -0,0 +1,10 @@
+story = "once upon a time there was a coder named Himanshu who wanted to become full stack developer"
+
+#String Functions :
+print(len(story))
+print(story.endswith("full"))
+print(story.count("c"))
+print(story.capitalize())
+print(story.find("upon"))
+print(story.replace("Himanshu", "Rajput"))
+ 
