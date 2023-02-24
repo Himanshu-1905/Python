a77b5d6e64b673c29c35160f2c89e735e1835762
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-24 12:14:32 +0530

    Add files via upload

---
+++ Dictionary_Syntax.py
@@ -0,0 +1,12 @@
+myDict = {
+    "Fast": "In a Quick Manner",
+    "Himanshu": "A Coder",
+    "Marks": [1, 2, 5],
+    "anotherdict": {'himanshu': 'Player'}
+}
+
+print(myDict['Fast'])
+print(myDict['Himanshu'])
+myDict['Marks'] = [45, 78]
+print(myDict['Marks'])
+print(myDict['anotherdict']['himanshu'])
