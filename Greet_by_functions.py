53190570c90deeb9e0bdad2a4aaa28b6ad8ec7f5
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-10 17:39:09 +0530

    Add files via upload

---
+++ Greet_by_functions.py
@@ -0,0 +1,9 @@
+# Write a program to greet a user with "Good day" using functions
+
+def greet(name):
+    print("Good Day, "+ name)
+
+def mySum(num1, num2):
+    return num1 + num2
+
+greet("Himanshu")
